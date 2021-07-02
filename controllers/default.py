046d5674db9bc62f8c83fe536b65822b8c9aced4
ChristianDiesta <64411885+ChristianDiesta@users.noreply.github.com>
2021-07-02 12:02:22 +0800

    Added rank function

--- controllers/default.py
+++ controllers/default.py
@@ -26,3 +26,23 @@ def uro():
     total_units = 234
 
     return locals()
+
+def rank():
+
+    students_honor = db(db.awards.award_title).select(
+            db.student.last_name, db.student.first_name,
+            db.student.middle_name, db.student.classification,
+            db.grade.total_units, db.grade.sum_of_grades,
+            db.grade.final_gwa, db.awards.award_title, orderby=db.grade.final_gwa,
+            left=[db.grade.on(db.student.student_id == db.grade.student_id),
+            db.awards.on(db.grade.award_id == db.awards.award_id)])
+
+    all_students = db(db.student.classification is None).select(
+            db.student.last_name, db.student.first_name,
+            db.student.middle_name, db.student.classification,
+            db.grade.total_units, db.grade.sum_of_grades,
+            db.grade.final_gwa, db.awards.award_title,
+            left=[db.grade.on(db.student.student_id == db.grade.student_id),
+            db.awards.on(db.grade.award_id == db.awards.award_id)])
+
+    return locals()
