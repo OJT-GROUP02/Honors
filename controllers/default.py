f18bddbdc039a169f7f43d352e064c5cd8042538
iveejntln <65966844+iveejntln@users.noreply.github.com>
2021-07-04 20:20:39 +0800

    fixed pep8 warnings

--- controllers/default.py
+++ controllers/default.py
@@ -10,7 +10,7 @@ def uro():
             db.grade.total_units, db.grade.sum_of_grades,
             db.grade.final_gwa, db.awards.award_title,
             left=[db.grade.on(db.student.student_id == db.grade.student_id),
-            db.awards.on(db.grade.award_id == db.awards.award_id)],
+                  db.awards.on(db.grade.award_id == db.awards.award_id)],
             orderby=db.student.last_name)
 
     female_students = \
@@ -20,29 +20,29 @@ def uro():
             db.grade.total_units, db.grade.sum_of_grades,
             db.grade.final_gwa, db.awards.award_title,
             left=[db.grade.on(db.student.student_id == db.grade.student_id),
-            db.awards.on(db.grade.award_id == db.awards.award_id)],
+                  db.awards.on(db.grade.award_id == db.awards.award_id)],
             orderby=db.student.last_name)
 
     total_units = 234
 
     return locals()
 
-def rank():
 
+def rank():
     students_honor = db(db.awards.award_title).select(
-            db.student.last_name, db.student.first_name,
-            db.student.middle_name, db.student.classification,
-            db.grade.total_units, db.grade.sum_of_grades,
-            db.grade.final_gwa, db.awards.award_title, orderby=db.grade.final_gwa,
-            left=[db.grade.on(db.student.student_id == db.grade.student_id),
-            db.awards.on(db.grade.award_id == db.awards.award_id)])
+        db.student.last_name, db.student.first_name,
+        db.student.middle_name, db.student.classification,
+        db.grade.total_units, db.grade.sum_of_grades,
+        db.grade.final_gwa, db.awards.award_title, orderby=db.grade.final_gwa,
+        left=[db.grade.on(db.student.student_id == db.grade.student_id),
+              db.awards.on(db.grade.award_id == db.awards.award_id)])
 
     all_students = db(db.student.classification is None).select(
-            db.student.last_name, db.student.first_name,
-            db.student.middle_name, db.student.classification,
-            db.grade.total_units, db.grade.sum_of_grades,
-            db.grade.final_gwa, db.awards.award_title,
-            left=[db.grade.on(db.student.student_id == db.grade.student_id),
-            db.awards.on(db.grade.award_id == db.awards.award_id)])
+        db.student.last_name, db.student.first_name,
+        db.student.middle_name, db.student.classification,
+        db.grade.total_units, db.grade.sum_of_grades,
+        db.grade.final_gwa, db.awards.award_title,
+        left=[db.grade.on(db.student.student_id == db.grade.student_id),
+              db.awards.on(db.grade.award_id == db.awards.award_id)])
 
     return locals()
