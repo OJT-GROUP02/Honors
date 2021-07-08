919c2c17e19ac8618a87b976545ad55278815257
Maedel Joice <79438281+Maedel@users.noreply.github.com>
2021-07-08 17:56:49 +0800

    added table header, fixed column range
    from 7 to 6

--- controllers/default.py
+++ controllers/default.py
@@ -130,14 +130,14 @@ ws.page_setup.paperWidth = '8.5in'
 
 # Heading
 ws.append(['Republic of the Philippines'])
-ws.merge_cells('A1:G1')
+ws.merge_cells('A1:F1')
 ws.append(['Bicol University'])
-ws.merge_cells('A2:G2')
+ws.merge_cells('A2:F2')
 ws.append([college_name_upper])
-ws.merge_cells('A3:G3')
+ws.merge_cells('A3:F3')
 ws['A3'].font = Font(bold=True)
 ws.append([college_address])
-ws.merge_cells('A4:G4')
+ws.merge_cells('A4:F4')
 
 for row in ws.iter_rows():
     for cell in row:
@@ -152,7 +152,7 @@ for row in ws.iter_rows():
 
 # Date
 date = str(datetime.datetime.today().strftime('%B %d, %Y'))
-date_today = ws['G5']
+date_today = ws['F5']
 date_today.value = date
 date_today.alignment = Alignment(horizontal='right')
 
@@ -162,16 +162,16 @@ ws['A7'].value = "THE BICOL UNIVERSITY REGISTRAR"
 ws['A8'].value = "Bicol University"
 ws['A9'].value = "Legazpi City"
 
-for row in ws['A7':'G9']:
+for row in ws['A7':'F9']:
     for cell in row:
         cell.alignment = Alignment(horizontal='left')
         
 #Attention   
-for col in range(1, 8):
+for col in range(1, 7):
     ws.column_dimensions[get_column_letter(col)].bestFit=True
 
 ws['A12'].value = "Attn: UNIVERITY EVALUATION/REVIEW COMMITTEE ON HONOR AND GRADUATES"
-ws.merge_cells('A12:G12')
+ws.merge_cells('A12:F12')
 ws['A12'].alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
 
 #Greetings
@@ -185,21 +185,38 @@ for row in range (16, 18):
    
 ws['A16'].value = "Herewith are the Official List of Candidates for Graduation with Honors under the different degree programs"
 ws['A16'].alignment = Alignment(horizontal='left', indent=1)
-ws.merge_cells('A16:G16')
+ws.merge_cells('A16:F16')
 
 ws['A17'].value = f" of the {college_name}  for the {college_semester}, {academic_year} "
 ws['A17'].alignment = Alignment(horizontal='left')
-ws.merge_cells('A17:G17')
+ws.merge_cells('A17:F17')
 
 #table
 
 thin = Side(border_style="thin", color="000000")# border style, color 
 border = Border(left=thin, right=thin, top=thin, bottom=thin)# the position of the border 
 
-rows = ws.iter_cols(min_row=19, min_col=1, max_row=86, max_col=7)
+rows = ws.iter_cols(min_row=19, min_col=1, max_row=86, max_col=6)
 
 for row in rows:
     for cell in row:
         cell.border = border
+
+rows = ws.iter_cols(min_row=19, min_col=1, max_row=86, max_col=6)
+
+for row in rows:
+    for cell in row:
+        cell.alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
+
+ws['A19'].value = "NO"
+ws['B19'].value = "NAME"
+ws.merge_cells(start_row=19, start_column=2, end_row=20, end_column=2)
+ws['C19'].value = "RATING"
+ws.merge_cells('C19:E19')
+ws['C20'].value = "Sum of Grades"
+ws['D20'].value = "Total Units"
+ws['E20'].value = "Final GWA"
+ws['F19'].value = "AWARD"
+ws.merge_cells(start_row=19, start_column=6, end_row=20, end_column=6)
 
 wb.save('static/Honors.xlsx')
